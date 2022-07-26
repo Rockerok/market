d548413591522d1523a6873f5200059236f78b12
Rockerok <Rockerok@mail.ru>
2022-07-26 22:03:31 +0300

    HW_12_подготовка

--- src/main/java/ru/gb/market/configs/ApplicationConfiguration.java
+++ src/main/java/ru/gb/market/configs/ApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package ru.gb.market.config;
+package ru.gb.market.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
